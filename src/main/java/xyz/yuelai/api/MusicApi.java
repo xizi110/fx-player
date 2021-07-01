cae53c9368e821aff2ea29b8350ba2f622fbc8a4
zhong <1103884823@qq.com>
2021-07-01 17:33:59 +0800

    网易云接口更新

--- src/main/java/xyz/yuelai/api/MusicApi.java
+++ src/main/java/xyz/yuelai/api/MusicApi.java
@@ -1,19 +1,21 @@
 package xyz.yuelai.api;
 
+import com.dtflys.forest.annotation.BaseRequest;
 import com.dtflys.forest.annotation.GetRequest;
 import com.dtflys.forest.annotation.Query;
 
 /**
  * @author zhong
  * @date 2021-03-17 15:02:10 周三
+ * 需要部署网易云接口，此处使用 网易云音乐 Node.js API service
+ * GitHub 地址 https://github.com/xizi110/NeteaseCloudMusicApi
  */
+@BaseRequest(baseURL = "https://netease-cloud-music-api-ten-orcin.vercel.app")
 public interface MusicApi {
 
-//    @GetRequest(url = "http://musicapi.leanapp.cn/search")
-    @GetRequest(url = "http://localhost:3000/search")
+    @GetRequest(url = "/search")
     String search(@Query("keywords") String keywords, int offset, int limit);
 
-//    @GetRequest(url = "https://api.imjad.cn/cloudmusic/?type=song&search_type=1")
-    @GetRequest(url = "http://localhost:3000/song/url")
+    @GetRequest(url = "/song/url")
     String detail(@Query("id") long id);
 }
